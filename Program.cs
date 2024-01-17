b726b5038e9fc9cb96e714b925cf6e4b8c855146
Ewere Diagboya <nimboya@gmail.com>
2024-01-17 12:25:05 +0100

    chore: update welcome message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Welcome to Pipeops ASP.NET Core Sample App!");
+app.MapGet("/", () => "Welcome to Pipeops ASP.NET Core Sample App! ");
 
 app.Run();
